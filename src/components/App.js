5248efd0e3659c69abbb3690246102d2c9318314
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-05 17:18:48 +0530

    countdown-timer

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,44 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  let [time, setTime] = useState(0);
+  const handleKeyDown = (event) => {
+      const input = document.getElementById("timeCount").value;
+      if ((isNaN(input)) || (input.length === 0) 
+      || (Number(input) === 0)) {
+        setTime(0);
+        return;
+  }
+  const key = event.keyCode;
+  if (key !== 13) {
+    return;
+  }
+  setTime(Math.floor(input));
+};
+const tick = () => {
+  if (time === 0 || time <= 0) {
+    return;
+  }
+  setTime(time - 1);
+};
+
+useEffect(() => {
+  const id = setInterval(tick, 1000);
+  return () => {
+    clearInterval(id);
+  };
+}, [time]);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKeyDown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{time}</div>
     </div>
   )
 }
-
 
 export default App;
